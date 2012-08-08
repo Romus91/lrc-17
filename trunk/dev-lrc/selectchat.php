bc33583f910723dd86ff7b6c8636f169c1f166ee
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-08-08 11:13:48 +0000

    Correction d'un bug d'affichage du chat quand le nombre d'article était inferieur à 15.

--- trunk/dev-lrc/selectchat.php
+++ trunk/dev-lrc/selectchat.php
@@ -2,7 +2,7 @@
 include_once("verif.php");
 include_once("pass.php");
 $nb=mysql_fetch_array(mysql_query("SELECT count(message) FROM chat"));
-$query=mysql_query("SELECT * FROM chat ORDER BY timestamp ASC LIMIT ".($nb[0]-15).",".$nb[0]."");
+$query=mysql_query("SELECT * FROM chat ORDER BY timestamp ASC LIMIT ".(($nb[0]<15)?0:$nb[0]-15).",".$nb[0]."");
 while ($mess=mysql_fetch_array($query))
 {
 	echo "<div class='small'>
