d26100b678e4a70681816306e5c438dac080840b
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-08-15 21:06:28 +0000

    debut de la gestion des membres en OO

--- dev-lrc/class/MemberController.class.php
+++ dev-lrc/class/MemberController.class.php
@@ -12,4 +12,22 @@ class MemberController{
 		}
 		return $tabMember;
 	}
+	public function fetchMembre($id){
+		$query = 'select * from membre where id = :id;';
+		$req = ConnectionSingleton::connect()->prepare($query);
+		$req->execute(array('id'=>$id));
+		$data = $req->fetch(PDO::FETCH_OBJ);
+		$membre = new Member();
+		$membre	->setId($data->id)
+				->setLogin($data->login)
+				->setPass($data->pass)
+				->setDate($data->date)
+				->setArgent($data->argent)
+				->setEmail($data->email)
+				->setMajTimestamp($data->majtimestamp)
+				->setWallTimestamp($data->walltimestamp)
+				->setTheme($data->theme)
+				->setXp($data->xp);
+		return $membre;
+	}
 }
