0d1730c74f9979d36ec8b5b89323e02d2a6b3496
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-08-26 22:32:53 +0000

    refonte du chat

--- trunk/dev-lrc/online.php
+++ trunk/dev-lrc/online.php
@@ -1,25 +1,24 @@
-				<?php 
-				
-				include_once("verif.php");
-				include_once("pass.php");
-				$loginOnline=mysql_fetch_array(mysql_query("SELECT id FROM online WHERE login =  '".$_SESSION['login']."'"));
-				$time=date("Y-m-j H:i:s");
-				if ($loginOnline['id'])
-				{
-					mysql_query("UPDATE online SET timestamp = '".$time."' WHERE login = '".$_SESSION['login']."'");
-				}else
-					mysql_query("INSERT INTO online (login) VALUES ('".$_SESSION['login']."')");
-					
-				$query=mysql_query("SELECT * FROM online");
-				$sql=mysql_query("SELECT * FROM online ORDER BY login ASC LIMIT 0,10");
-				while($delOnline=mysql_fetch_array($query))
-				{
-					if (((strtotime($time))-(strtotime($delOnline['timestamp']))) > 100)
-						mysql_query("DELETE FROM online WHERE id = ".$delOnline['id']."");
-				}
-					while ($query=mysql_fetch_array($sql))
-					{
-						echo $query['login']."<br>
-									";					
-					}
-				?>
+<?php
+
+include_once("verif.php");
+include_once("pass.php");
+$loginOnline=mysql_fetch_array(mysql_query("SELECT id FROM online WHERE login =  '".$_SESSION['login']."'"));
+$time=date("Y-m-j H:i:s");
+if ($loginOnline['id'])
+{
+	mysql_query("UPDATE online SET timestamp = '".$time."' WHERE login = '".$_SESSION['login']."'");
+}else
+mysql_query("INSERT INTO online (login) VALUES ('".$_SESSION['login']."')");
+
+$query=mysql_query("SELECT * FROM online");
+$sql=mysql_query("SELECT * FROM online ORDER BY login ASC LIMIT 0,10");
+while($delOnline=mysql_fetch_array($query))
+{
+	if (((strtotime($time))-(strtotime($delOnline['timestamp']))) > 100)
+	mysql_query("DELETE FROM online WHERE id = ".$delOnline['id']."");
+}
+while ($query=mysql_fetch_array($sql))
+{
+	echo $query['login']."<br>";
+}
+?>
