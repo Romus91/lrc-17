004fa957f0f9fc9f5ae0a0034ffd97018aa37024
romain.demoustier@gmail.com <romain.demoustier@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-08-28 16:51:24 +0000

    Mise à jour des jauges (pic/viergex.png)

--- trunk/dev-lrc/fin.php
+++ trunk/dev-lrc/fin.php
@@ -15,7 +15,7 @@
 				</td>
 			</tr>
 			<tr align="left">
-				<td colspan="2"><a href='http://romustech.dyndns.org'>Created by Romus</a><font color="FAC21D"> -- Copyright © LES RESCAPES DE CITE 17 -- </font></td>
+				<td colspan="2"><a href='http://romustech.dyndns.org'>Created by Romus</a><font color="FAC21D"> -- Copyright © LES RESCAPES DE CITE 17 -- Co-developper : Anthares <!-- Mets l'adresse de ta plateforme personnelle ici ;) --></font></td>
 			</tr> 
 		</table>
 	</td>
