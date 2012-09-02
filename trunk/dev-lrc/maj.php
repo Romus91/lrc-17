5eeb833f4eb511567d6400235c7893f9415c9852
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-09-02 14:44:21 +0000

    Ajout d'un système de vote sur les mises à jour

--- trunk/dev-lrc/maj.php
+++ trunk/dev-lrc/maj.php
@@ -1,105 +1,93 @@
 <?php include_once("verif.php");
 $id=mysql_fetch_array(mysql_query("SELECT id FROM membre WHERE login = '".$_SESSION['login']."'"));?>
-<center>
-	<table width='550' align='left'  class='small'>
-	<?php
-	if ($id[0] == 4)
+<table width='100%' align='left' class='small'>
+<?php if ($id[0] == 4):?>
+	<tr>
+		<td>
+			<form action='index.php?page=maj' method='post' name='wall'>
+				<table class='small' width='100%'>
+					<tr class="button">
+						<td id='button'><a href='index.php?page=maj'>ACTUALISER</a></td>
+					</tr>
+					<tr>
+						<td class='title2'><b>Message : </b>
+						</td>
+					</tr>
+					<tr>
+						<td align=center><textarea name='message' cols='60' rows='3'></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td align=center><input type='submit' name='go' value='Envoyer'>
+						</td>
+					</tr>
+				</table>
+				<script>document.wall.message.focus();</script>
+			</form>
+		</td>
+	</tr>
+	<?php if (($_POST['go']=='Envoyer') AND ($_POST['message'] <> ''))
 	{
-		echo "
-		<tr>
-			<td>
-			   <form action = 'index.php?page=maj' method='post' name='wall'>
-					<table class='small' width='100%'>
-						<tr>
-							<td>
-								<table class='button' width='100%'>
-									<tr>
-										<td id='button'>
-											<a href='index.php?page=maj'>ACTUALISER</a>
-										</td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td class='title2'>
-								<b>Message : </b>
-							</td>
-						</tr>
-						<tr>
-							<td align=center>
-								<textarea name='message' cols='60' rows='3' ></textarea>
-							</td>
-						</tr>
-						<tr>
-							<td align=center>
-								<input type='submit' name='go' value='Envoyer'>
-							</td>
-						</tr>
-					</table>
-					<script>document.wall.message.focus()</script>
-					</form>
-			</td>
-		</tr>";
-    if (($_POST['go']=='Envoyer') AND ($_POST['message'] <> ''))
-    {
 		//si tout a été bien rempli, on insère le message dans la table SQL
 		$sql = 'INSERT INTO maj(date,id_expediteur,message) VALUES( "'.date("H:i:s").'","'.$_SESSION['login'].'", "'.mysql_escape_string($_POST['message']).'")';
-        mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error());
+		mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error());
 		$sql = "UPDATE membre SET majtimestamp = (SELECT MAX( timestamp ) FROM maj) WHERE login ='".$_SESSION['login']."'";
 		mysql_query($sql);
-    }
- }
+	}
+	endif;
+
 	$sql = "UPDATE membre SET majtimestamp = (SELECT MAX( timestamp ) FROM maj) WHERE login ='".$_SESSION['login']."'";
 	mysql_query($sql);
 	// on prépare une requete SQL cherchant tous les dates, message ainsi que l'auteur des messages  par ordre décroissant en se limitant à 10 message
-    $sql = 'SELECT date,id_expediteur,message,id,timestamp FROM maj ORDER BY id DESC LIMIT 0,15 ';
+	$sql = 'SELECT date,id_expediteur,message,id,timestamp FROM maj ORDER BY id DESC LIMIT 0,15 ';
 	// lancement de la requete SQL
-	$res = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+	$res = ConnectionSingleton::connect()->prepare($sql);
+	$res->execute();
+	$data = $res->fetchAll(PDO::FETCH_OBJ);
+	$i=0;
+	?>
 
-	echo"
+	<tr>
+		<td align=center class='color4' colspan=2>&nbsp;</td>
+	</tr>
+	<?php
+	foreach ($data as $item):
+		$statement = 'select * from votemaj where id_maj = :maj and vote = :vote;';
 
-		<tr>
-			<td align=center class='color4'>&nbsp;</td>
-		</tr>
-		";
-	$i=1;
-	While($t=mysql_fetch_array($res))
-    {
-        if ($i <= $cpt)
-		{
-			echo"
+		$req = ConnectionSingleton::connect()->prepare($statement);
+		$req->execute(array('maj'=>$item->id,'vote'=>1));
+		$voteup = $req->rowCount();
 
-		<tr>
-			<td><font size = 3>",$t[4],"</font></td>
-		</tr>
-		<tr>
-			<td><font color=FF6600>",$t[2],"</font></td>
-		</tr>
-		<tr>
-			<td align=center class='color4'>&nbsp;</td>
-		</tr>
-				";
-		}
-        else
-		{
-			echo"
-		<tr>
-			<td><font size = 3>",$t[4],"</font></td>
-		</tr>
-		<tr>
-			<td>",$t[2],"</td>
-		</tr>
-		<tr>
-			<td align=center class='color4'>&nbsp;</td>
-		</tr>
-				";
-		}
-		$i++;
-    }
-    echo"
-	</table>
-</center>
-		";
-
-?>
+		$req = ConnectionSingleton::connect()->prepare($statement);
+		$req->execute(array('maj'=>$item->id,'vote'=>-1));
+		$votedown = $req->rowCount();
+		?>
+	<tr>
+	<?php if ($i < $cpt):?>
+		<td>
+			<p><font size=3><?php echo $item->date;?></font></p>
+			<p><font color=FF6600><?php echo $item->message?></font></p>
+		</td>
+	<?php else: ?>
+		<td>
+			<p><font size=3><?php echo $item->date;?></font></p>
+			<p><?php echo $item->message?></font></p>
+		</td>
+	<?php endif;?>
+		<td align=right width="100px" style="padding:5px;">
+			<div class="vote">
+				<div><a href="votemaj.php?maj=<?php echo $item->id;?>&vote=up"><img src="pic/thumb_up.png"></a></div>
+				<div align=center><font color="00cc00"><?php echo $voteup;?></font></div>
+			</div>
+			<div class="vote">
+				<div><a href="votemaj.php?maj=<?php echo $item->id;?>&vote=down"><img src="pic/thumb_down.png"></a></div>
+				<div align=center><font color="cc0000"><?php echo $votedown;?></font></div>
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td align=center class='color4' colspan=2>&nbsp;</td>
+	</tr>
+	<?php $i++;
+	endforeach;?>
+</table>
