72ef97997c7e971020583bcbf60a655c30de31b1
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2012-09-03 19:47:52 +0000

    Legère modif du design de la fiche de perso Adaptation de la taille des avatar générés

--- dev-lrc/levelup.php
+++ dev-lrc/levelup.php
@@ -36,7 +36,7 @@ if(isset($_GET['perso'])){
 			$_SESSION['erreur']=false;
 			$perso->levelUp($stat);
 			$persoController->savePerso($perso);
-			imagepng(genImg($perso->getAvatar().'.JPG',207,0,$perso->getLevel(),0),'ava/'.$perso->getId().'.png');
+			imagepng(genImg($perso->getAvatar().'.JPG',176,0,$perso->getLevel(),0),'ava/'.$perso->getId().'.png');
 			$log=new Log();
 			$log->insertLog("Level up",$_SESSION['member_id'],$perso->getId(),"+1 ".$statChosen);
 			echo "<script language='javascript' type='text/javascript'>window.location.replace('index.php?page=perso&perso=".$perso->getId()."');</script>";
