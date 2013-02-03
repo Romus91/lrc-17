36231de29bdc1b24e7d072c22ce0c60cf53048ea
n.sornin@gmail.com <n.sornin@gmail.com@ea02a7a4-4d25-6492-5d25-41b602487639>
2013-02-03 21:37:05 +0000

    FIX et Ajustements suite à la MAJ 2013-01

--- dev-lrc/class/Crab.class.php
+++ dev-lrc/class/Crab.class.php
@@ -4,7 +4,7 @@ class Crab extends Monster{
 		$this->setLevel((int)$level);
 		$this->setBaseExp(1);
 		$this->setBaseLife(5);
-		$this->setDamage(0.5);
-		$this->setEnergyCost(0.3);
+		$this->setDamage(0.2);
+		$this->setEnergyCost(0.2);
 	}
 }
--- dev-lrc/freq.php
+++ dev-lrc/freq.php
@@ -1,5 +1,5 @@
 <p><h1>Fréquentation du site</h1></p>
-<table class="small">
+<table class="small" width=100%>
 <?php
 require_once 'autoload.php';
 require_once 'cdnHelper.php';
@@ -15,9 +15,9 @@ $data = $req->fetchAll(PDO::FETCH_OBJ);
 
 foreach ($data as $i):?>
 <tr>
-	<td width="100" style="border: 1px black solid"><?php echo date("d/m/Y",strtotime($i->date));?></td>
-	<td width="200" style="border: 1px black solid"><img src="<?php echo convertToCDNUrl('pic/jblanc.png');?>" width="<?php echo ($i->count/$max*100)?>%" height="10"></td>
-	<td width="30" style="border: 1px black solid"><?php echo $i->count;?></td>
+	<td width="100px" style="border: 1px black solid"><?php echo date("d/m/Y",strtotime($i->date));?></td>
+	<td style="border: 1px black solid"><img src="<?php echo convertToCDNUrl('pic/jblanc.png');?>" width="<?php echo ($i->count/$max*100)?>%" height="10"></td>
+	<td width="30px" style="border: 1px black solid"><?php echo $i->count;?></td>
 </tr>
 <?php endforeach;?>
 </table>
